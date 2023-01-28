198ccda3ed820c06620a09f4bde7092bc96a96ae
Jonathan <jonathanmoorevla@gmail.com>
2023-01-28 17:25:24 -0600

    Pass all tests

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,17 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+
+  def grades
+    by_grades = Student.all.order(grade: :desc)
+    render json: by_grades 
+  end
+
+
+
+  def show 
+    id_student = Student.find(params[:id])
+    render json: id_student
+  end
+
 end
